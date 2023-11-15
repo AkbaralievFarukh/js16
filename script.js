8d2d5a60eae3205a3713c2b5d5aa852eb20baf53
Farukh <akbfarukh@gmail.com>
2023-11-15 21:44:42 +0600

    add recept images with api

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const search = document.querySelector('#search')
 const currentCocktail = document.querySelector('#currentCocktail')
 const searchSubmit = document.querySelector('#search-submit')
 const apiUrl = 'https://www.thecocktaildb.com/api/json/v2/1/popular.php'
+const apiUrlImg = 'https://www.thecocktaildb.com/images/ingredients/'
 
 const getCocktail = () => {
     row.classList.remove('hidden')
@@ -49,6 +50,11 @@ const getSearchCocktail = () => {
         .then(dataSearch => {
             hideLoading();
             dataSearch.drinks.forEach(cocktail => {
+                let ingridient1 = cocktail.strIngredient1
+                let ingridient2 = cocktail.strIngredient2
+                let ingridient3 = cocktail.strIngredient3
+                let ingridient4 = cocktail.strIngredient4
+                console.log(`${apiUrlImg}${ingridient1}.png`)
                 currentCocktail.innerHTML += `
                         <div class="col-4">
                             <div class="card">
@@ -57,6 +63,24 @@ const getSearchCocktail = () => {
                                     <h2 class="card-title">${cocktail.strDrink}</h2>
                                     <p class="card-text">${cocktail.strAlcoholic}</p>
                                     <p class="card-text">${cocktail.strInstructions}</p>
+                                </div>
+                                <div class="grid">
+                                    <div class="div1">
+                                        <h4>${ingridient1}</h4>
+                                        <img src="${apiUrlImg}${ingridient1}.png" class="receptImg">
+                                    </div>
+                                    <div class="div2">
+                                        <h4>${ingridient2}</h4>
+                                        <img src="${apiUrlImg}${ingridient2}.png" class="receptImg">
+                                    </div>
+                                    <div class="div3">
+                                        <h4>${ingridient3}</h4>
+                                        <img src="${apiUrlImg}${ingridient3}.png" class="receptImg">
+                                    </div>
+                                    <div class="div4">
+                                        <h4>${ingridient4}</h4>
+                                        <img src="${apiUrlImg}${ingridient4}.png" class="receptImg">
+                                    </div>
                                 </div>
                             </div>
                         </div>
